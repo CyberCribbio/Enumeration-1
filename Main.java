e57c93b20ca9b22dcf74248c5549b20d3d7408e4
Cristian Valenti <122291593+CyberCribbio@users.noreply.github.com>
2023-02-08 17:12:17 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Month[] var1 = Month.values();
+        int var2 = var1.length;
+
+        for (int var3 = 0; var3 < var2; ++var3) {
+            Month month = var1[var3];
+            System.out.println("" + month + (month.toString().endsWith("Y") ? " ends" : " doesn't end") + " with y");
+        }
+
+    }
+}
